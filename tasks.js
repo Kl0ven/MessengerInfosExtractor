0e413924968b1e7188b272ef77b254244056cdf6
Bearcorrupted <jloup.m@gmail.com>
2019-08-18 14:07:03 +0200

    pie chart generator v0.4.0

--- tasks.js
+++ tasks.js
@@ -1,4 +1,6 @@
 const utf8 = require('utf8');
+const fs = require('fs');
+const chartExporter = require('highcharts-export-server');
 
 function getPseudoList (file, output) {
 	var msgs = file.messages;
@@ -32,6 +34,8 @@ function getNumbersOfMsgPerUser (file, output) {
 	var participants = file.participants;
 	var msgs = file.messages;
 	var resultats = {};
+	let pieData = [];
+	chartExporter.initPool();
 
 	for (let i in participants) {
 		let p = participants[i];
@@ -45,7 +49,48 @@ function getNumbersOfMsgPerUser (file, output) {
 
 	for (var p in resultats) {
 		output.write(utf8.decode(p) + ', ' + resultats[p] + '\n');
+		pieData.push({name: utf8.decode(p), y: resultats[p]});
 	}
+	const chartDetails = {
+		type: 'png',
+		scale: 2,
+		options: {
+			chart: {
+				type: 'pie'
+			},
+			title: {
+				text: 'Messages per user'
+			},
+			plotOptions: {
+				pie: {
+					dataLabels: {
+						enabled: true,
+						format: '<b>{point.name}</b>: {point.y}'
+					}
+				}
+			},
+			series: [
+				{
+					data: pieData
+				}
+			]
+		}
+	};
+	chartExporter.export(chartDetails, (err, res) => {
+		if (err) {
+			console.log(err);
+		}
+		// Get the image data (base64)
+		let imageb64 = res.data;
+		// Filename of the output
+		let outputFile = 'Outputs/Messages_per_User.png';
+		// Save the image to file
+		fs.writeFileSync(outputFile, imageb64, 'base64', function (err) {
+			if (err) console.log(err);
+		});
+		console.log('Saved image!');
+		chartExporter.killPool();
+	});
 }
 
 function getMostReactedMessage (file, output) {
