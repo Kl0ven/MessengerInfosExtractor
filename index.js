9c3f7ed88c3f806910a4034882c86c2e8c4cf4fc
Bearcorrupted <jloup.m@gmail.com>
2019-08-19 13:03:14 +0200

    adding color support

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const tasks = require('./tasks.js');
 const chartExporter = require('highcharts-export-server');
+const chalk = require('chalk');
 
 var messageFilename = 'message_1.json';
-
-console.log('\n Starting ... \n');
 var contents = fs.readFileSync(messageFilename);
 var file = JSON.parse(contents);
 
@@ -18,22 +17,22 @@ runTasks(file, [tasks.getPseudoList,
 async function runTasks (file, tasks) {
 	chartExporter.initPool();
 	for (var t in tasks) {
-		console.log(`Starting :${tasks[t].name}...`);
+		console.log(`${chalk.cyan('Starting')} :${chalk.yellow(tasks[t].name)}...`);
 		var output = fs.createWriteStream(`Outputs/${tasks[t].name}.txt`);
 		output.on('error', (err) => { console.log('error', err); });
 		let chartDetails = tasks[t](file, output);
 		output.end();
 		if (chartDetails !== undefined) {
-			await exportDataToPie(chartExporter, chartDetails, `Outputs/${tasks[t].name}.${chartDetails.type}`).catch(console.log);
+			await exportDataToPie(chartExporter, chartDetails, `Outputs/${tasks[t].name}.${chartDetails.type}`).catch((err) => console.log(chalk.red(err)));
 		}
-		console.log(`Finished :${tasks[t].name} !`);
+		console.log(`${chalk.green('Finished')} :${chalk.yellow(tasks[t].name)} !`);
 	}
 	chartExporter.killPool();
 }
 
 async function exportDataToPie (chartExporter, chartDetails, path) {
 	return new Promise((resolve, reject) => {
-		console.log('Exporting data to pie chart!');
+		console.log(chalk.cyan('Exporting data to pie chart!'));
 		chartExporter.export(chartDetails, (err, res) => {
 			if (err) {
 				reject(err);
@@ -45,7 +44,7 @@ async function exportDataToPie (chartExporter, chartDetails, path) {
 			fs.writeFileSync(path, imageb64, 'base64', function (err) {
 				if (err) console.log(err);
 			});
-			console.log('Image saved !');
+			console.log(chalk.magenta('Image saved !'));
 			resolve();
 		});
 	});
